fb1ba49274114e8b08163d6794d028acfb7939f0
Mohammed Swofvan P <swofvanmhd@gmail.com>
2026-02-28 16:52:13 +0530

    logout function added

--- Rydo/auth_app/urls.py
+++ Rydo/auth_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('user_signup/', views.user_signup, name='user_signup'),
     path('driver_signup/', views.driver_signup, name='driver_signup'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- Rydo/auth_app/views.py
+++ Rydo/auth_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
@@ -9,7 +9,6 @@ from django.contrib.auth import authenticate
 from .models import User, Driver
 from .forms import UserSignupForm, DriverSignupForm
 from .serializers import UserSerializer, DriverSerializer
-
 
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -43,37 +42,15 @@ def driver_signup(request):
         driver = Driver.objects.get(user=driver_user)
         serializer = DriverSerializer(driver)
 
-        return Response({"message": "Driver signup request sent. Waiting for admin approval", "data": serializer.data}, status=201)
+        return Response({
+            "message": "Driver signup request sent. Waiting for admin approval",
+            "data": serializer.data
+            },
+            status=201)
 
     return Response(form.errors, status=400)
 
 # ------------------------------------------------------------------------   login
-
-# @api_view(['POST'])
-# @permission_classes([AllowAny])
-# def login(request):
-
-#     user = authenticate(
-#         username=request.data.get('email'),
-#         password=request.data.get('password')
-#     )
-
-#     if not user:
-#         return Response({"error": "Invalid email or password"}, status=401)
-    
-#     try:
-#         driver = Driver.objects.get(user=user)
-
-#         if driver.status != 'accepted':
-#             return Response(
-#                 {"error": "Driver account not approved by admin"},
-#                 status=403
-#             )
-
-#     except Driver.DoesNotExist:
-#         pass
-
-#     return Response({"message": "Login success"}, status=200)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -106,3 +83,32 @@ def login(request):
         "access": str(refresh.access_token),
         "refresh": str(refresh),
     }, status=200)
+
+
+# ------------------------------------------------------------------------   login
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    try:
+        refresh_token = request.data.get("refresh")
+
+        if not refresh_token:
+            return Response(
+                {"error": "Refresh token is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        token = RefreshToken(refresh_token)
+        token.blacklist()
+
+        return Response(
+            {"message": "Logout successful"},
+            status=status.HTTP_205_RESET_CONTENT
+        )
+
+    except Exception:
+        return Response(
+            {"error": "Invalid token"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
